1232062685ca5de0674f288d0d333a1a1e29806e
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-05-12 16:50:41 -0400

    fixed the problem with USA in task 2

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -24,7 +24,7 @@ class Contact extends Component {
                 >
                     <Text style= {{marginBottom: 1}}>1 Nucamp Way</Text>
                     <Text style= {{marginBottom: 1}}>Seattle, WA 98001</Text>
-                    <Text style= {{marginBottom: 10}}> U.S.A.</Text>
+                    <Text style= {{marginBottom: 10}}>U.S.A.</Text>
 
                     <Text style= {{marginBottom: 1}}>Phone: 1-206-555-1234</Text>
                     <Text style= {{marginBottom: 1}}>Email: campsites@nucamp.co</Text>
