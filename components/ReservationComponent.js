921d81ba752b88eaa80b2b46156e35a59586940b
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-06-02 21:06:41 -0400

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -11,6 +11,7 @@ import {
 } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Animatable from "react-native-animatable";
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
   state = {
@@ -35,8 +36,11 @@ class Reservation extends Component {
           onPress: () => this.resetForm()
         },
         {
-          text: "OK",
-          onPress: () => this.resetForm()
+          text: 'OK',
+          onPress: () => {
+              this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+              this.resetForm();
+          }
         }
       ],
       { cancelable: false }
@@ -50,8 +54,35 @@ class Reservation extends Component {
       showCalendar: false
     });
   }
+
+  async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
+
   render() {
-    return (
+      return (
       <ScrollView>
         <Animatable.View animation="zoomIn" duration={2000} delay={1000}>
           <View style={styles.formRow}>
